12c023bb030c7657a48931dd74c4f7a33f444007
Anton <gh@gets.love>
2025-01-21 14:08:38 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 import { access, symlink } from "node:fs/promises";
 import { join } from "node:path";
 
-let userConfig = undefined
-
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -74,30 +66,14 @@ const nextConfig = {
       })(),
     );
 
+    if (isServer) {
+      config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    } else {
+      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    }
+
     return config;
   },
-}
-
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
 }
 
 export default nextConfig
